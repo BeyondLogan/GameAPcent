6847ab6ffeffb1a74365a534fe12c71c542482f0
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-01 13:10:00 +0930

    Create Update2.sh

---
+++ Update2.sh
@@ -0,0 +1,31 @@
+sudo yum check update
+
+sudo yum update
+
+sudo yum check-upgrade
+
+sudo yum upgrade
+
+sudo yum check-update | more
+
+sudo yum check-update | grep kernel
+
+sudo yum check-update
+
+uname -mrs
+
+sudo yum update
+
+sudo yum --security update
+
+sudo yum update pkg_name
+
+sudo yum update nginx
+
+sudo yum update php
+
+sudo yum update httpd
+
+sudo yum -x 'kernel*' -x 'php*' update
+
+udo reboot
