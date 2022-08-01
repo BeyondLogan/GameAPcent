1c1d281bbfedabfc0925f3e2062eef8530b28099
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-01 13:38:02 +0930

    Update SetUp.sh

--- SetUp.sh
+++ SetUp.sh
@@ -9,3 +9,35 @@ sudo yum install -y wget software-properties-common ca-certificates apt-transpor
 sudo yum-y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
 
 sudo yum install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo apt-get -y install \
+    php7.3-common \
+    php7.3-cli \
+    php7.3-fpm \
+    php7.3-pdo \
+    php7.3-mysql \
+    php7.3-redis \
+    php7.3-curl \
+    php7.3-bz2 \
+    php7.3-zip \
+    php7.3-xml \
+    php7.3-mbstring \
+    php7.3-bcmath \
+    php7.3-gmp \
+    php7.3-intl
+
+sudo yum -y install \
+    php7.3-common \
+    php7.3-cli \
+    php7.3-fpm \
+    php7.3-pdo \
+    php7.3-mysql \
+    php7.3-redis \
+    php7.3-curl \
+    php7.3-bz2 \
+    php7.3-zip \
+    php7.3-xml \
+    php7.3-mbstring \
+    php7.3-bcmath \
+    php7.3-gmp \
+    php7.3-intl
