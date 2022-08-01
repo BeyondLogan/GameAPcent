cf1565cdb7df5c40114783169be3eefea671be1e
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-01 13:24:26 +0930

    Create SetUp.sh

---
+++ SetUp.sh
@@ -0,0 +1,11 @@
+cd
+
+curl -sLO http://packages.gameap.ru/installer.sh && bash installer.sh
+
+sudo apt-get install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo yum install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo yum-y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo yum install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
