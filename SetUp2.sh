71f0a382f91322c3ba059aff2ccba6260471072f
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-01 13:56:54 +0930

    Update SetUp2.sh

--- SetUp2.sh
+++ SetUp2.sh
@@ -15,3 +15,32 @@ git clone https://github.com/et-nik/gameap /var/www/gameap
 cd /var/www/gameap
 
 cp .env.12345678 .env
+
+composer install --no-dev --optimize-autoloader
+
+php artisan key:generate --force
+
+php artisan migrate --seed
+
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install -y nodejs
+
+npm install
+
+yum install Npm -y
+
+npm run prod
+
+* * * * * cd /patch/to/gameap && php artisan schedule:run >> /dev/null 2>&1
+
+cd /patch/to/gameap && php artisan schedule:run >> /dev/null 2>&1
+
+touch /var/www/gameap/database.sqlite
+
+DB_CONNECTION=sqlite
+DB_DATABASE=/var/www/gameap/database.sqlite
+
+php artisan migrate --seed
+
+cd public
+php -S localhost:8080
