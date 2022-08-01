0c98b04dbeca984f73b0098985e0d368a810afac
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-01 13:17:01 +0930

    Create Update3.sh

---
+++ Update3.sh
@@ -0,0 +1,21 @@
+uname -mrs
+
+sudo yum check-update
+
+sudo yum install git
+
+git --version
+
+sudo rpm --import http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
+
+sudo yum install git
+
+sudo yum install epel-release
+
+sudo yum install screen
+
+systemctl get-default
+
+systemctl isolate graphical.target
+
+sudo Reboot
