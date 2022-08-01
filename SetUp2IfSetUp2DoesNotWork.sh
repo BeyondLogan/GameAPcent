2bf5e9c2cb644740aad7f12f9d9ade9bfc8d9c3a
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-01 14:00:05 +0930

    Create SetUp2IfSetUp2DoesNotWork.sh

---
+++ SetUp2IfSetUp2DoesNotWork.sh
@@ -0,0 +1,44 @@
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+wget -O - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt-get update
+
+sudo yum install git
+
+git clone https://github.com/et-nik/gameap /var/www/gameap
+
+cd /var/www/gameap
+
+cp .env.12345678 .env
+
+composer install --no-dev --optimize-autoloader
+
+php artisan key:generate --force
+
+php artisan migrate --seed
+
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install -y nodejs
+
+npm install
+
+yum install Npm -y
+
+npm run prod
+
+* * * * * cd /patch/to/gameap && php artisan schedule:run >> /dev/null 2>&1
+
+cd /patch/to/gameap && php artisan schedule:run >> /dev/null 2>&1
+
+touch /var/www/gameap/database.sqlite
+
+DB_CONNECTION=sqlite
+DB_DATABASE=/var/www/gameap/database.sqlite
+
+php artisan migrate --seed
+
+cd public
+php -S localhost:8080
